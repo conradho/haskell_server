cc8113f58da77edb5a944866689c2ba4dda32030
conrad <conrad@pythonanywhere.com>
2015-03-18 23:11:18 +0000

    made a sample library and imported it in testrunner

---
+++ src-lib/WarmupExercises.hs
@@ -0,0 +1,4 @@
+
+module WarmupExercises where
+
+
--- src-test/TestRunner.hs
+++ src-test/TestRunner.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import Test.QuickCheck
+import WarmupExercises as W
 
 main = do
     quickCheck ((\s -> s == s) :: [Char] -> Bool)
