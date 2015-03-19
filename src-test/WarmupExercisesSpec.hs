2dec84d3abd8024a4c465e175d56c227b6c9feca
conrad <conrad@pythonanywhere.com>
2015-03-19 19:49:52 +0000

    wow used quickcheck property

--- src-test/WarmupExercisesSpec.hs
+++ src-test/WarmupExercisesSpec.hs
@@ -1,20 +1,20 @@
 module WarmupExercisesSpec where
 
 import Test.Hspec
-import Test.QuickCheck
+import Test.QuickCheck (property)
 
--- import WarmupExercises (wordStartsWithA, selectElementsOfListStartingWithA)
+import WarmupExercises (wordStartsWithA, selectElementsOfListStartingWithA)
 
 checkAllStartWithA :: [String] -> Bool
-checkAllStartWithA x = True
+checkAllStartWithA x = and $ map wordStartsWithA x
 
 spec :: Spec
 spec = do
     describe "selectElementsOfListSTartingWithA" $ do
         it "should be blah" $
             1 `shouldBe` 1
-        it "should only have 'a's in the list" $
-            1 `shouldBe` 2
+        it "should only have 'a's in the list" $ property $
+            checkAllStartWithA . selectElementsOfListStartingWithA
     describe "second function" $ do
         it "should only have 'a's in the list" $
             1 `shouldBe` 2
