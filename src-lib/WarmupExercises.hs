e1530b39169984f589490cdb675a7e8dc21645d4
conrad <conrad@pythonanywhere.com>
2015-03-20 01:38:41 +0000

    made quicksort and sortComponentsAndList helper functions

--- src-lib/WarmupExercises.hs
+++ src-lib/WarmupExercises.hs
@@ -1,13 +1,26 @@
 module WarmupExercises where
 
+-- help functions
 wordStartsWithA :: String -> Bool
 wordStartsWithA (x:xs) = x == 'a'
 wordStartsWithA [] = False
 
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (pivot:rest) =
+    let smallerSorted = quicksort [e | e <- rest, e <= pivot]
+        biggerSorted = quicksort [e | e <- rest, e > pivot]
+    in smallerSorted ++ [pivot] ++ biggerSorted
+
+sortComponentsAndList :: Ord a => [[a]] -> [[a]]
+-- if there was a [] pattern, it needs to be first here, because otherwise the strings pattern matches everything
+sortComponentsAndList strings = quicksort $ map quicksort strings
+
+-- first question
 selectElementsOfListStartingWithA :: [String] -> [String]
 selectElementsOfListStartingWithA words = filter wordStartsWithA words
 
 -- concatenate pairs of strings
 everyPossiblePair :: [String] -> [String]
-everyPossiblePair (x:xs) = [x ++ other | other <- xs] ++ everyPossiblePair xs
 everyPossiblePair [] = []
+everyPossiblePair (x:xs) = sortComponentsAndList $ [x ++ other | other <- xs] ++ everyPossiblePair xs
